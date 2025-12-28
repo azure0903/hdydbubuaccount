fec373d5ea486edcbd965da2c04724431b4f499e
azure0903 <azure0903@gmail.com>
2025-12-28 21:03:00 +0900

    Add functionality to access Google Sheets

---
+++ sheets.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+@st.cache_resource
+def get_gspread_client():
+    creds = Credentials.from_service_account_info(
+        st.secrets["google_service_account"],
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+    return gspread.authorize(creds)
+
+
+def open_sheet(sheet_name):
+    gc = get_gspread_client()
+    return gc.open(sheet_name)
